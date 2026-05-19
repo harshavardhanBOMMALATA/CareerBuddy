c2d248463b59901a18c978077cfe2f7e70a70895
HARSHAVARDHAN_BOMMALATA <167017461+harshavardhanBOMMALATA@users.noreply.github.com>
2026-05-19 13:42:18 +0530

    Update settings.py

--- careerbuddy/settings.py
+++ careerbuddy/settings.py
@@ -21,6 +21,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
